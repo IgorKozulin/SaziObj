40cb0ca8e7d5d3cb0ad5b266b8a498d339ce8e8b
Aleksey Emelianov <aleksey.emelianov@evraz.com>
2021-04-08 23:08:12 +0700

    add visible obj

--- Distance/Assets/Scripts/DistanseCube.cs
+++ Distance/Assets/Scripts/DistanseCube.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class DistanseCube : MonoBehaviour
 {
     public Transform player;
+    public Text posBlock;
     public Text posPlayer;
+
+    private bool visible = false;
 
     void Start()
     {
@@ -15,7 +18,23 @@ public class DistanseCube : MonoBehaviour
 
     void Update()
     {
+        posBlock.text = "позиция блока: \n" + this.transform.position.ToString() + "\n";
+        posBlock.text += "длина = " + this.transform.localScale.x.ToString() + "\n";
+        posBlock.text += "ширина = " + this.transform.localScale.y.ToString() + "\n";
+        posBlock.text += "высота = " + this.transform.localScale.z.ToString() + "\n";
+
         posPlayer.text = "позиция камеры: \n" + player.position.ToString() + "\n";
         posPlayer.text += "дистанция от камеры: \n" + Vector3.Distance(player.position, transform.position) + " м\n";
+        posPlayer.text += visible ? "объект виден" : "объект не виден";
+    }
+
+    private void OnBecameVisible()
+    {
+        visible = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        visible = false;
     }
 }

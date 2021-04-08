310f7c19d1ae07158c46d68a4d6520c41bedbf97
Aleksey Emelianov <aleksey.emelianov@evraz.com>
2021-04-08 20:55:02 +0700

    добавил проет

---
+++ Distance/Assets/Scripts/DistanseCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanseCube : MonoBehaviour
+{
+    public Transform player;
+    public Text posPlayer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        posPlayer.text = "позиция камеры: \n" + player.position.ToString() + "\n";
+        posPlayer.text += "дистанция от камеры: \n" + Vector3.Distance(player.position, transform.position) + " м\n";
+    }
+}

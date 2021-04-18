b8593f6069f3e75c60303a113d04d9cb5e04297b
Aleksey Emelianov <aleksey.emelianov@evraz.com>
2021-04-18 12:04:32 +0700

    поправки по лучам

--- Distance/Assets/Scripts/Luch.cs
+++ Distance/Assets/Scripts/Luch.cs
@@ -48,12 +48,15 @@ public class Luch : MonoBehaviour
 
                     if (Physics.Raycast(ray[i, j], out hit))
                     {
-                            // point точка куда попала
-                            // normal нормали
-                            // distance дистанция
-                            // например вычислить точку столкновения
-                        print(hit.point);
-                        text += hit.distance + " " + ((this.transform.position.x - hit.point.x) * -1) + " " + ((this.transform.position.y - hit.point.y) * -1) + " | ";
+                        // point точка куда попала
+                        // normal нормали
+                        // distance дистанция
+                        // например вычислить точку столкновения
+                        if (i == 0 && j == 0)
+                        {
+                            print("point  " + hit.point);
+                        }
+                        text += hit.distance + " " + ((transform.position.x - hit.point.x))+ " " + ((transform.position.y - hit.point.y)) + " | ";
                     }
                     else
                     {
@@ -63,6 +66,26 @@ public class Luch : MonoBehaviour
                 sw.WriteLineAsync(text);
             }
             sw.Close();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Ray ray1;
+            Vector3 rotation = new Vector3(transform.forward.x, transform.forward.y, transform.forward.z);
+            ray1 = new Ray(this.transform.position, rotation); // создаем лучь в позиции камеры
+            Debug.DrawRay(this.transform.position, rotation * 50f, Color.green); // отрисовка луча начальные кординаты и цвет
+
+            if (Physics.Raycast(ray1, out hit))
+            {
+                print("debug  " + hit.point);
+                print(ray1.origin);
+                print("=  " + (ray1.origin.x - hit.point.x) + " " + (ray1.origin.y - hit.point.y) + " " + (ray1.origin.z - hit.point.z));
+            }
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            print("------------00");
         }
     }
 }
